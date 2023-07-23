1e818d4dac1fc38da461d9a7183146519907fbdf
Vsevolod <106489473+Settle23@users.noreply.github.com>
2023-07-23 22:11:03 +0300

    still works

---
+++ src/main/java/web/config/DbConfig.java
@@ -0,0 +1,11 @@
+package web.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@ComponentScan(value = "web")
+public class DbConfig {
+}
