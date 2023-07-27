7c5eefd9be799f882b2c43111a0ed5ccc6f168ff
Vsevolod <106489473+Settle23@users.noreply.github.com>
2023-07-27 23:38:45 +0300

    перенес транзакции в сервис слой

--- src/main/java/web/dao/UserDaoIMPL.java
+++ src/main/java/web/dao/UserDaoIMPL.java
@@ -14,31 +14,29 @@ public class UserDaoIMPL implements UserDao {
     @PersistenceContext
     private EntityManager entityManager;
 
-    @Transactional
     @Override
     public List<User> usersList() {
         return entityManager.createQuery("SELECT u From User u", User.class).getResultList();
     }
 
-    @Transactional
+
     @Override
     public User getUser(long id) {
         return entityManager.find(User.class, id);
     }
 
-    @Transactional
     @Override
     public void adduser(User user) {
         entityManager.persist(user);
     }
 
-    @Transactional
+
     @Override
     public void delete(long id) {
         entityManager.remove(entityManager.find(User.class, id));
     }
 
-    @Transactional
+
     @Override
     public void update(User user) {
         entityManager.merge(user);
--- src/main/java/web/service/UserServiceImpl.java
+++ src/main/java/web/service/UserServiceImpl.java
@@ -2,6 +2,7 @@ package web.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import web.dao.UserDao;
 import web.model.User;
 
@@ -16,26 +17,31 @@ public class UserServiceImpl implements UserService {
         this.userDao = userDao;
     }
 
+    @Transactional
     @Override
     public List<User> usersList() {
         return userDao.usersList();
     }
 
+    @Transactional
     @Override
     public User getUser(long id) {
         return userDao.getUser(id);
     }
 
+    @Transactional
     @Override
     public void adduser(User user) {
         userDao.adduser(user);
     }
 
+    @Transactional
     @Override
     public void delete(long id) {
         userDao.delete(id);
     }
 
+    @Transactional
     @Override
     public void update(User user) {
         userDao.update(user);
